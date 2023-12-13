b03688ceb4e4842dce3e509cca26d8cd136dea61
Emdinga <emdinga@gmail.com>
2023-12-13 01:42:17 -0800

    debad

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-#!/usr/bin/Python3
+#!/usr/bin/python3
+""" debug app """
 
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/create-event')
+def create_event():
+    return render_template('create_event.html')
 
+@app.route('/event-created')
+def event_created():
+    return render_template('event_created.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
