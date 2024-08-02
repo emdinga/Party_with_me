82cb01723706a4f4c4ad174b37216fb5d52c1707
Emdinga <emdinga@gmail.com>
2024-08-02 09:25:14 +0200

    updating script

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const modalLinks = document.querySelectorAll('[data-modal]');
-    const modals = document.querySelectorAll('.modal');
-    const closeButtons = document.querySelectorAll('.close-button');
+document.addEventListener('DOMContentLoaded', function () {
+    const seeMoreLink = document.querySelector('.see-more-link');
+    const seeMoreSection = document.querySelector('.see-more');
+    const termsModal = document.getElementById('termsModal');
+    const privacyModal = document.getElementById('privacyModal');
 
-    modalLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            const modalId = link.getAttribute('data-modal') + '-modal';
-            document.getElementById(modalId).style.display = 'flex';
-        });
+    seeMoreLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        if (seeMoreSection.style.display === 'block') {
+            seeMoreSection.style.display = 'none';
+            seeMoreLink.textContent = 'See More';
+        } else {
+            seeMoreSection.style.display = 'block';
+            seeMoreLink.textContent = 'See Less';
+            seeMoreSection.scrollIntoView({ behavior: 'smooth' });
+        }
     });
 
-    closeButtons.forEach(button => {
-        button.addEventListener('click', () => {
+    document.querySelectorAll('.close-button').forEach(button => {
+        button.addEventListener('click', function () {
             button.closest('.modal').style.display = 'none';
         });
     });
 
-    window.addEventListener('click', (event) => {
-        if (event.target.classList.contains('modal')) {
-            event.target.style.display = 'none';
-        }
-    });
-})
+    window.showModal = function (modalId) {
+        document.getElementById(modalId).style.display = 'flex';
+    };
 
+    window.hideModal = function (modalId) {
+        document.getElementById(modalId).style.display = 'none';
+    };
+});
