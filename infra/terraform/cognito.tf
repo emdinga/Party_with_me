7e0ab6af4ee7289a514e4615868ae9693e2f5d1a
Emdinga <emdinga@gmail.com>
2025-11-23 10:43:02 +0200

    fixing cognito tf file

--- infra/terraform/cognito.tf
+++ infra/terraform/cognito.tf
@@ -1,9 +1,6 @@
-# Cognito User Pool
-# -------------------------------
 resource "aws_cognito_user_pool" "party_with_me_users" {
   name = "party-with-me-userpool"
 
-  # Optional: email verification
   auto_verified_attributes = ["email"]
 
   password_policy {
@@ -21,11 +18,8 @@ resource "aws_cognito_user_pool" "party_with_me_users" {
     }
   }
 
-  schema {
-    name                = "username"
-    attribute_data_type = "String"
-    required            = true
-  }
+  # DO NOT DEFINE username schema!
+  # Cognito username is built-in. Do not create a custom attribute.
 
   schema {
     name                = "email"
@@ -36,38 +30,4 @@ resource "aws_cognito_user_pool" "party_with_me_users" {
   admin_create_user_config {
     allow_admin_create_user_only = false
   }
-}
-
-# -------------------------------
-# Cognito User Pool Client
-# -------------------------------
-resource "aws_cognito_user_pool_client" "party_with_me_client" {
-  name            = "party-with-me-client"
-  user_pool_id    = aws_cognito_user_pool.party_with_me_users.id
-  generate_secret = false
-
-  explicit_auth_flows = [
-    "ALLOW_USER_PASSWORD_AUTH",
-    "ALLOW_REFRESH_TOKEN_AUTH",
-    "ALLOW_USER_SRP_AUTH"
-  ]
-
-  prevent_user_existence_errors = "ENABLED"
-
-  # Optional: configure callback URLs for hosted UI
-  callback_urls = [
-    "http://party-with-me-frontend.s3-website-us-east-1.amazonaws.com/members_home.html",
-  ]
-
-  logout_urls = [
-    "http://party-with-me-frontend.s3-website-us-east-1.amazonaws.com/index.html",
-  ]
-}
-
-# -------------------------------
-# Optional: Cognito Domain
-# -------------------------------
-resource "aws_cognito_user_pool_domain" "party_with_me_domain" {
-  domain       = "party-with-me-auth" # must be globally unique
-  user_pool_id = aws_cognito_user_pool.party_with_me_users.id
 }
