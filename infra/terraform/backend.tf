bb71c155d24523f141285ce8a955d7b8dfbbacb6
Emdinga <emdinga@gmail.com>
2025-12-02 16:33:27 +0200

    running fmt

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "party-terraform-state-711387123965"
-    key    = "party/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "party-terraform-state-711387123965"
+    key            = "party/terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
-    encrypt = true
+    encrypt        = true
   }
 }
