edf3762c8f7e30c972f7e8d7398f0ca56998c5e5
Emdinga <emdinga@gmail.com>
2025-12-02 16:12:00 +0200

    adding api url in the login page

--- frontend_site/assets/js/login.js
+++ frontend_site/assets/js/login.js
@@ -5,7 +5,7 @@ document.getElementById("login-form").addEventListener("submit", async function 
     const password = document.getElementById("password").value;
 
     try {
-        const response = await fetch("https://d3bpj9bucrhmjl.cloudfront.net/login", {
+        const response = await fetch("https://2og2qwei66.execute-api.us-east-1.amazonaws.com/prod/", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email, password })
