70166c94534e4e0a5378516361d933cc7368b197
Emdinga <emdinga@gmail.com>
2025-12-02 15:34:51 +0200

    mograting statefile to remote

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "your-terraform-state-bucket"
+    bucket = "party-terraform-state-711387123965"
     key    = "party/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-locks"
