56a80700ea96fc6c512a46e60a31952b2580c435
Emdinga <emdinga@gmail.com>
2025-12-03 14:49:08 +0200

    updating backend

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "party-terraform-state-711387123965"
-    key            = "party/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket  = "party-terraform-state-711387123965"
+    key     = "party/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+    # dynamodb_table = "terraform-locks"  <-- removed to disable locking
   }
 }
